185d30717beee5df772e50e6467ba193f8993ccb
Ravi Makhija <ravi.makhija.pi@gmail.com>
2023-04-28 00:06:06 -0400

    Add header font with layout

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    KGHappy: require('./assets/fonts/KGHAPPY.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Quizzy.News</Text>
+      <Text style={{ fontFamily: 'KGHappy', fontSize: 46, marginTop: 40 }}>QUIZZY.NEWS</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +25,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });

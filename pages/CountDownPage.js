f10d52b86f8a2ac9e341bf85543f1f21e9aae8cd
Clifton <clifga@gmail.com>
2023-08-26 13:43:21 -0400

    new timer design

--- pages/CountDownPage.js
+++ pages/CountDownPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
 import { View, Text, StyleSheet } from "react-native";
 import { styled } from "nativewind";
@@ -7,18 +7,28 @@ const StyledView = styled(View);
 const StyledText = styled(Text);
 
 export default function CountDownPage({ navigation }) {
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      navigation.navigate("GamePage");
-    }, 3000);
+  const [countdown, setCountdown] = useState(3);
 
-    // Clean up the timeout when the component is unmounted or the effect is re-run
+  useEffect(() => {
+    if (countdown === 0) {
+      navigation.navigate("GamePage");
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setCountdown(countdown - 1);
+    }, 1000);
+
     return () => clearTimeout(timeout);
-  }, []);
+  }, [countdown, navigation]);
 
   return (
     <StyledView className="flex-1 items-center justify-center bg-[#ded1e4]">
-      <StyledView className="w-[263px] h-[263px] rounded-full border-8 border-white"></StyledView>
+      <StyledView className="w-[363px] h-[363px] rounded-full border-[40px] border-white flex items-center justify-center">
+        <StyledText className="text-center text-white text-[200px] font-bold leading-normal">
+          {countdown}
+        </StyledText>
+      </StyledView>
     </StyledView>
   );
 }

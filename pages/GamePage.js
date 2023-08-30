2e27ee5ca36eca65d7d2ce9c5733ed19a370c3f9
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2023-08-29 18:57:14 -0700

    abstracted question

--- pages/GamePage.js
+++ pages/GamePage.js
@@ -12,20 +12,27 @@ export default function GamePage() {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [question, setQuestion] = useState(data);
   const [score, setScore] = useState(0);
+  const [choices,setChoices] = useState([])
   // let obj = Object.freeze({ score: 0 });
+
 
   useEffect(() => {
     setQuestion(data[currentQuestion]);
-  }, []);
-
+    setChoices([...data[currentQuestion].choices])
+  }, [currentQuestion]);
+  
   console.log(question);
 
   const handleAnswer = (choice) => {
-    if (choice === question[currentQuestion].answer) {
+    console.log(question.answer)
+    if (choice === question.answer) {
       setSelectedAnswer("correct");
+      setScore(score + 1)
     } else {
       setSelectedAnswer("incorrect");
     }
+    if (currentQuestion + 1 < data.length) {setCurrentQuestion(currentQuestion + 1)}
+    else { console.log('score page')}
   };
 
   if (!question) {
@@ -36,37 +43,29 @@ export default function GamePage() {
     <StyledView className="flex-1 items-center justify-center bg-[#ded1e4]">
       <StyledView className="display-flex justify-center items-center">
         <StyledText className="text-3xl font-bold">
-          Who was the first president of the US?
+         {question.question}
         </StyledText>
         <StyledText>{score}</StyledText>
       </StyledView>
       <StyledView className="space-y-4">
-        <Pressable
-          className="w-[335px] h-[95px] bg-white hover:bg-[#80C9FA] rounded-[10px] shadow-[0 35px 60px -15px rgba(0,0,0,0.3)]"
-          onPress={() => handlePress("George")}
-        >
-          <StyledText
-            className="text-center font-bold
-          justify-center items-center content-center
-          
-          "
-          >
-            George
-          </StyledText>
-        </Pressable>
-        <Pressable
-          className="w-[335px] h-[95px] bg-white hover:bg-[#80C9FA] rounded-[10px] shadow-[0 35px 60px -15px rgba(0,0,0,0.3)]"
-          onPress={() => handlePress("Button 1")}
-        >
-          <StyledText>Mountain Dew Elizonda The Third</StyledText>
-        </Pressable>
-        <Pressable
-          className="w-[335px] h-[95px] bg-white hover:bg-[#80C9FA] rounded-[10px] shadow-[0 35px 60px -15px rgba(0,0,0,0.3)]"
-          onPress={() => handlePress("Button 2")}
-        >
-          <StyledText>Joe Maldanado</StyledText>
-        </Pressable>
+        {/* Write unique id for json  */}
+        
+        {choices.map((choice, idx)=>{
+          return(
+            <Pressable
+              key = {idx}
+              className="w-[335px] h-[95px] bg-white hover:bg-[#80C9FA] rounded-[10px] shadow-[0 35px 60px -15px rgba(0,0,0,0.3)]"
+              onPress={() => handleAnswer(choice)}>
+              <StyledText
+                className="text-center font-bold justify-center items-center content-center">
+                 {choice}
+                </StyledText>
+
+            </Pressable>
+          )
+        })}
       </StyledView>
     </StyledView>
   );
 }
+

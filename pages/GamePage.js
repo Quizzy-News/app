41209cd801ce7c33e2b5b166898e60e2808faba6
Clifton <clifga@gmail.com>
2023-09-07 11:48:50 -0400

    timer without css

--- pages/GamePage.js
+++ pages/GamePage.js
@@ -7,32 +7,43 @@ import { styled } from "nativewind";
 const StyledView = styled(View);
 const StyledText = styled(Text);
 
-export default function GamePage() {
+export default function GamePage({navigation}) {
+  const [countdown, setCountdown] = useState(60);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [question, setQuestion] = useState(data);
   const [score, setScore] = useState(0);
-  const [choices,setChoices] = useState([])
-  // let obj = Object.freeze({ score: 0 });
-
+  const [choices, setChoices] = useState([]);
 
   useEffect(() => {
     setQuestion(data[currentQuestion]);
-    setChoices([...data[currentQuestion].choices])
-  }, [currentQuestion]);
-  
-  console.log(question);
+    setChoices([...data[currentQuestion].choices]);
+
+    if (countdown === 0) {
+      navigation.navigate("ScorePage");
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setCountdown(countdown - 1);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [countdown, navigation, currentQuestion]);
 
   const handleAnswer = (choice) => {
-    console.log(question.answer)
+    console.log(question.answer);
     if (choice === question.answer) {
       setSelectedAnswer("correct");
-      setScore(score + 1)
+      setScore(score + 1);
     } else {
       setSelectedAnswer("incorrect");
     }
-    if (currentQuestion + 1 < data.length) {setCurrentQuestion(currentQuestion + 1)}
-    else { console.log('score page')}
+    if (currentQuestion + 1 < data.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      console.log("score page");
+    }
   };
 
   if (!question) {
@@ -42,30 +53,29 @@ export default function GamePage() {
   return (
     <StyledView className="flex-1 items-center justify-center bg-[#ded1e4]">
       <StyledView className="display-flex justify-center items-center">
+        <StyledText className="text-3xl font-bold">{countdown}</StyledText>
         <StyledText className="text-3xl font-bold">
-         {question.question}
+          {question.question}
         </StyledText>
         <StyledText>{score}</StyledText>
       </StyledView>
       <StyledView className="space-y-4">
         {/* Write unique id for json  */}
-        
-        {choices.map((choice, idx)=>{
-          return(
-            <Pressable
-              key = {idx}
-              className="w-[335px] h-[95px] bg-white hover:bg-[#80C9FA] rounded-[10px] shadow-[0 35px 60px -15px rgba(0,0,0,0.3)]"
-              onPress={() => handleAnswer(choice)}>
-              <StyledText
-                className="text-center font-bold justify-center items-center content-center">
-                 {choice}
-                </StyledText>
 
+        {choices.map((choice, idx) => {
+          return (
+            <Pressable
+              key={idx}
+              className="w-[335px] h-[95px] bg-white hover:bg-[#80C9FA] rounded-[10px] shadow-[0 35px 60px -15px rgba(0,0,0,0.3)]"
+              onPress={() => handleAnswer(choice)}
+            >
+              <StyledText className="text-center font-bold justify-center items-center content-center">
+                {choice}
+              </StyledText>
             </Pressable>
-          )
+          );
         })}
       </StyledView>
     </StyledView>
   );
 }
-
--- routes/MyStack.js
+++ routes/MyStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../pages/Home';
 import CountDownPage from '../pages/CountDownPage';
 import GamePage from '../pages/GamePage';
+import ScorePage from '../pages/ScorePage';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function MyStack() {
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="CountDownPage" component={CountDownPage} />
       <Stack.Screen name="GamePage" component={GamePage} />
+      <Stack.Screen name="ScorePage" component={ScorePage} />
     </Stack.Navigator>
   );
 }

ed80b3e3d845809a76437e0154cf3e53369cec46
Caleb Cleghorn <caleb@cleghorn.org>
2023-09-27 14:28:51 -0500

    added custom fonts and styled Home page

--- pages/Home.js
+++ pages/Home.js
@@ -1,11 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { useFonts } from "expo-font";
-import { NavigationContainer } from "@react-navigation/native";
-import { createAppContainer } from "@react-navigation/native";
-import { styled } from "nativewind";
-// const StyledView = styled(View);
-const StyledText = styled(Text);
 
 export default function Home({ navigation }) {
   const [loaded] = useFonts({
@@ -21,114 +15,31 @@ export default function Home({ navigation }) {
   const pressHandler = () => {
     navigation.navigate("CountDownPage");
   };
+
   return (
-    <View style={styles.screenContainer}>
-      <View style={styles.topContainer}>
-        <Text style={styles.topTextPrimary}>QUIZZY.NEWS</Text>
+    <View className="h-screen flex items-center bg-white">
+      <View className="mt-10 p-3 w-screen">
+        <Text className="text-4xl font-kghappy font-bold">QUIZZY.NEWS</Text>
+        <Text className="font-bold font-lexend">a daily quiz game on current events</Text>
+        <Text className="font-bold font-lexend">from stories published very recently</Text>
       </View>
-      <View style={styles.secondContainer}>
-        <Text style={styles.topTextSecondary}>
-          a daily quiz game on current events
-        </Text>
-        <Text style={styles.topTextSecondary}>
-          from stories published very recently
-        </Text>
-      </View>
-      <View style={styles.middleContainer}>
-        <Text style={styles.middleText}>Ready?</Text>
-        <Pressable style={styles.playButton} onPress={pressHandler}>
-          <StyledText
-            className="text-white text-[35px] font-bold tracking-[4px]"
+      <View className="mt-36 p-3 w-screen">
+        <Text className="text-xl font-lexend">Ready?</Text>
+        <Pressable onPress={pressHandler} className="p-3 mt-6 bg-[#80C9FA] w-full flex items-center rounded-lg">
+          <Text
+            className="text-white text-4xl font-bold"
           >
             PLAY
-          </StyledText>
+          </Text>
         </Pressable>
       </View>
-      <View
-        style={{
-          borderBottomColor: "#909090",
-          borderBottomWidth: StyleSheet.hairlineWidth,
-          marginBottom: 15,
-        }}
-      />
-      <View style={styles.bottomContainer}>
-        <Text style={styles.bottomText}>QUIZZY.NEWS</Text>
-        <Text style={styles.bottomText}>ABOUT</Text>
-        <Text style={[styles.bottomText, { marginBottom: 20 }]}>
+      <View className="p-3 w-screen mt-auto">
+        <Text className="text-[#909090] font-bold">ABOUT</Text>
+        <Text className="text-[#909090] font-bold">QUIZZY.NEWS</Text>
+        <Text className="text-[#909090]">
           Ⓒ 2022 EMISQWE
         </Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-  },
-  topContainer: {
-    flex: 2,
-    backgroundColor: "#fff",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    marginBottom: 50,
-  },
-  middleContainer: {
-    flex: 6,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  secondContainer: {
-    marginTop: 30,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  bottomContainer: {
-    marginTop: 20,
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "flex-end",
-    alignItems: "flex-start",
-  },
-  topTextPrimary: {
-    fontFamily: "KGHappy",
-    fontSize: 46,
-    marginTop: 70,
-  },
-  topTextSecondary: {
-    fontFamily: "Lexend",
-    fontSize: 14,
-    marginLeft: -80,
-  },
-  middleText: {
-    fontFamily: "Lexend",
-    fontSize: 14,
-    marginLeft: -270,
-    marginTop: -40,
-  },
-  playButton: {
-    width: 350,
-    marginTop: 10,
-    backgroundColor: "#80C9FA",
-    alignItems: "center",
-    borderRadius: 6,
-    // shadowColor: '#53ADF0',
-    // shadowOffset: { width: 0, height: 12 }
-  },
-  playButtonText: {
-    fontFamily: "Lexend",
-    fontSize: 35,
-    paddingVertical: 3,
-    color: "#fff",
-  },
-  bottomText: {
-    fontFamily: "Lexend",
-    fontSize: 12,
-    color: "#909090",
-    marginLeft: 40,
-  },
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kghappy': ['KGHappy', 'sans-serif'],
+        'lexend': ['Lexend', 'sans-serif'],
+        'jost': ['Jost', 'sans-serif']
+    },
+    },
   },
   plugins: [],
 };

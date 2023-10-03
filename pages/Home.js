628efed6b34424d9df16b8ee4ba18fbf112a6c4c
Caleb Cleghorn <caleb@cleghorn.org>
2023-10-03 10:59:48 -0500

    added global color variables

--- pages/Home.js
+++ pages/Home.js
@@ -18,25 +18,30 @@ export default function Home({ navigation }) {
 
   return (
     <View className="h-screen flex items-center bg-white">
-      <View className="mt-10 p-3 w-screen">
-        <Text className="text-4xl font-kghappy font-bold">QUIZZY.NEWS</Text>
-        <Text className="font-bold font-lexend">a daily quiz game on current events</Text>
-        <Text className="font-bold font-lexend">from stories published very recently</Text>
+      <View className="mt-6 p-3 w-auto flex items-center">
+        <Text className="text-[44px] font-kghappy color-[#3D3D3D]">QUIZZY.NEWS</Text>
+        <Text className="pl-3 font-lexend-black self-start text-[#3D3D3D]">
+          a daily quiz game on current events{"\n"}
+          from stories published very recently
+        </Text>
       </View>
       <View className="mt-36 p-3 w-screen">
-        <Text className="text-xl font-lexend">Ready?</Text>
-        <Pressable onPress={pressHandler} className="p-3 mt-6 bg-[#80C9FA] w-full flex items-center rounded-lg">
-          <Text
-            className="text-white text-4xl font-bold"
-          >
+        <Text className="pl-3 text-xl font-bold font-lexend text-[#3D3D3D]">
+          Ready?
+        </Text>
+        <Pressable onPress={pressHandler} className="group h-12 file:mt-6 w-full flex items-center justify-center rounded-lg active:h-16">
+          <View className="h-12 bg-[#53ADF0] w-full rounded-lg absolute top-3" />
+          <View className="h-12 bg-[#80C9FA] w-full rounded-lg absolute" />
+          <Text className="text-white text-4xl font-black">
             PLAY
           </Text>
         </Pressable>
       </View>
-      <View className="p-3 w-screen mt-auto">
-        <Text className="text-[#909090] font-bold">ABOUT</Text>
-        <Text className="text-[#909090] font-bold">QUIZZY.NEWS</Text>
-        <Text className="text-[#909090]">
+      <View className="w-screen mt-auto">
+        <View className="h-[1px] w-screen bg-[#909090]" />
+        <Text className="pl-3 pt-4 text-[#909090] font-bold">ABOUT</Text>
+        <Text className="pl-3 pt-2 text-[#909090] font-bold">QUIZZY.NEWS</Text>
+        <Text className="pl-3 pt-2 pb-5 text-[#909090]">
           Ⓒ 2022 EMISQWE
         </Text>
       </View>

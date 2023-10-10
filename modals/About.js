b7e69e78f6c608a0945c4afb846ca175431cbc7c
Caleb Cleghorn <caleb@cleghorn.org>
2023-10-10 15:22:46 -0500

    finished styling for about modal

--- modals/About.js
+++ modals/About.js
@@ -1,16 +1,21 @@
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 
 export default function AboutModal({ navigation }) {
 
     return (
-        <View className="flex items-center justify-center bg-[#DED1E4]">
+        <View className="h-screen p-3 flex items-center justify-center bg-[#DED1E4]">
+            <View className="top-20 right-5 absolute">
+                <AntDesign
+                    onPress={() => navigation.goBack()}
+                    name="close" size={24} color="black" />
+            </View>
             <Text className="font-lexend text-2xl">
                 Quizzy.news gives you a quick quiz on the latest top headlines, everyday!
                 {"\n"}
                 {"\n"}
                 You have 60 seconds to answer 5 questions. Be quick and accurate for the highest score.
             </Text>
-            <Button onPress={() => navigation.goBack()} title="Dismiss" />
         </View>
     );
 }

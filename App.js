45257eed766b9f256345931a4675d28634aed4ff
Caleb Cleghorn <caleb@cleghorn.org>
2023-10-14 15:45:40 -0500

    cleanup

--- App.js
+++ App.js
@@ -7,18 +7,6 @@ export default function App() {
 
   return (
     <>
-      <style type="text/css">{`
-        @font-face {
-          font-family: 'MaterialIcons';
-          src: url(${require("react-native-vector-icons/Fonts/MaterialIcons.ttf")}) format('truetype');
-        }
-
-        @font-face {
-          font-family: 'FontAwesome';
-          src: url(${require("react-native-vector-icons/Fonts/FontAwesome.ttf")}) format('truetype');
-        }
-      `}</style>
-
       <NavigationContainer>
         <MyStack />
       </NavigationContainer>

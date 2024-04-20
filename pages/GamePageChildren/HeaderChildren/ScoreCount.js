8178b8c6db6d01b1605107fa7e17fe22848d6a43
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2024-04-20 15:31:22 -0700

    formats score with leading '0's to match '000' pattern

--- pages/GamePageChildren/HeaderChildren/ScoreCount.js
+++ pages/GamePageChildren/HeaderChildren/ScoreCount.js
@@ -4,8 +4,11 @@ import { styled } from 'nativewind';
 
 const StyledText = styled(Text);
 
+
 export default function ScoreCount({ score }) {
+
+    const formattedScore = score.toString().padStart(3 ,'0')
     return (
-        <StyledText className="font-lexend-bold text-xl text-white m-3 p-1">{score}</StyledText>
+        <StyledText className="font-lexend-bold text-xl text-white m-3 p-1">{formattedScore}</StyledText>
     )
 }

4979d54060645457f147b74589057baae5da3331
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2024-04-20 15:30:43 -0700

    fixed footer to use new colors, but also this isn't used on the gamepage anymore, can be refined for homepage

--- pages/GamePageChildren/Footer.js
+++ pages/GamePageChildren/Footer.js
@@ -7,10 +7,14 @@ const StyledText = styled(Text);
 
 export default function Footer() {
     return (
-        <StyledView className="mt-auto">
-            <StyledView className="bg-grey-2 mb-[10px] h-[1px]"/>
-            <StyledText className="font-lexend-bold text-grey-2 text-[12px] pl-[10px] pt-[5px]">QUIZZY.NEWS</StyledText>
-            <StyledText className="font-lexend text-grey-2 pl-[10px] pt-[5px] pb-[5px]">Ⓒ 2022 EMISQWE</StyledText>
+        <StyledView className="mt-auto"
+            style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}
+            >
+            <StyledView className="bg-grey-2 mb-[10px] h-[1px] "/>
+            <StyledText className="font-lexend-bold text-grey-2 text-[12px] pl-[10px] pt-[5px] "
+                onPress={() => navigation.navigate('AboutModal')}
+                >QUIZZY.NEWS</StyledText>
+            <StyledText className="font-lexend text-grey-2 pl-[10px] pt-[5px] pb-[5px] ">Ⓒ 2024 EMISQWE</StyledText>
         </StyledView>
     )
 }

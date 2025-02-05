e142adc9bb3cfde8b401c502fb31a521c96765a1
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2025-02-05 06:07:54 -0800

    small changes to be compliant with useFont

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import "react-native-gesture-handler";
-import * as React from "react";
+import {useEffect} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MyStack from "./routes/MyStack";
+import * as SplashScreen from 'expo-splash-screen'
 import { useFonts } from "expo-font";
-import "./nativewind-output";
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
 
-  const [loaded] = useFonts({
-    KGHappy: require("./public/fonts/KGHAPPY.ttf"),
-    LexendRegular: require("./public/fonts/Lexend-Regular.ttf"),
-    LexendBold: require("./public/fonts/Lexend-Bold.ttf"),
-    LexendBlack: require("./public/fonts/Lexend-Black.ttf"),
-    Jost: require("./public/fonts/Jost-Regular.ttf"), 
-    FuturaMedium: require("./public/fonts/Futura-Medium.otf"),
+  const [loaded, error] = useFonts({
+    'KGHappy': require("./public/fonts/KGHAPPY.ttf"),
+    'LexendRegular': require("./public/fonts/Lexend-Regular.ttf"),
+    'LexendBold': require("./public/fonts/Lexend-Bold.ttf"),
+    'LexendBlack': require("./public/fonts/Lexend-Black.ttf"),
+    'Jost': require("./public/fonts/Jost-Regular.ttf"), 
+    'FuturaMedium': require("./public/fonts/Futura-Medium.otf"),
   });
   
-
-  if (!loaded) {
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen
+    }
+  }, [loaded, error]);
+  
+  if (!loaded && !error) {
     console.log("Fonts did not load")
     return null;
   }
